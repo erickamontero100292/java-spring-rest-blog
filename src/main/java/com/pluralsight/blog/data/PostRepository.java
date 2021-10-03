e830c1dd2e8c70c0b9002de5a8b35185d031cb2a
eamontero <monteroh.ericka@gmail.com>
2021-10-02 22:01:34 -0500

    Add Author Relationships and Data

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -2,12 +2,16 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-
+@RepositoryRestResource(exported = false)
 public interface PostRepository   extends JpaRepository<Post, Long> {
+
     @RestResource(rel="contains-title", path="containsTitle")
     List<Post> findByTitleContaining (String title);
+
+    public List<Post> findByAuthor_Lastname(String lastname);
 }
